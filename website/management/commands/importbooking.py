eafb5f3026306d2e2b0caa3fcf231b285822d596
up2002099 <up2002099@myport.ac.uk>
2023-04-27 11:54:45 +0100

    update importbooking

--- website/management/commands/importbooking.py
+++ website/management/commands/importbooking.py
@@ -8,29 +8,43 @@ from django.db.models import Q
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        with open(r'C:\Users\pound\OneDrive\propertyManagementSystem\apr12_apr19.csv', 'r', encoding='utf-8') as csvfile:
+        with open(r'C:\Users\pound\OneDrive\propertyManagementSystem\apr27_may03.csv', 'r', encoding='utf-8') as csvfile:
             reader = csv.reader(csvfile)
             rowHeader = next(reader)
             
             # rename original header rows
+            # rowHeader[0] = 'guestName'
+            # rowHeader[1] = 'numGuests'
+            # rowHeader[2] = 'checkInDate'
+            # rowHeader[3] = 'checkOutDate'
+            # rowHeader[4] = 'resRoomType'
+            # rowHeader[5] = 'reservationStatus'
+            # rowHeader[6] = 'totalPayment'
+            # rowHeader[7] = 'commission'
+            # rowHeader[8] = 'extReservationId'
+            
             rowHeader[0] = 'guestName'
             rowHeader[1] = 'numGuests'
             rowHeader[2] = 'checkInDate'
             rowHeader[3] = 'checkOutDate'
             rowHeader[4] = 'resRoomType'
-            rowHeader[5] = 'reservationStatus'
-            rowHeader[6] = 'totalPayment'
-            rowHeader[7] = 'commission'
-            rowHeader[8] = 'extReservationId'
-            rowHeader[9] = 'smartFlex'
+            rowHeader[5] = 'totalPayment'
+            rowHeader[6] = 'commission'
+            rowHeader[7] = 'extReservationId'
+            rowHeader[8] = 'reservationStatus'
+            
+            # rowHeader[9] = 'smartFlex'
 
             for row in reader:
                 # THB 0 means no payment (etc guest cancelled) = remove from list
                 if 'THB 0' not in row:
+                    for i in range(9):
+                        print(rowHeader[i], row[i])
+                    
                     resRoomType = row[4]
                     numGuests = row[1]
-                    totalPayment = row[6]
-                    commission = row[7]
+                    totalPayment = row[5]
+                    commission = row[6]
                     numSingle = 0
                     numTwin = 0
                     
@@ -38,8 +52,8 @@ class Command(BaseCommand):
                     row[1] = int(numGuests.split()[0])
                     
                     # format to decimalField - THB is always before the decimalField
-                    row[6] = Decimal(totalPayment.replace('THB ', '').replace(',',''))
-                    row[7] = Decimal(commission.replace('THB ', '').replace(',',''))
+                    row[5] = Decimal(totalPayment.replace('THB ', '').replace(',', ''))
+                    row[6] = Decimal(commission.replace('THB ', '').replace(',', ''))
                     
                     # double ifs as multiple rooms can be composed of multiple room types
                     if 'Single Bed Room' in resRoomType:
@@ -56,12 +70,18 @@ class Command(BaseCommand):
                             numTwin = int(resRoomType.split()[0])
                         elif ',' in resRoomType: # Twin Bed Room is always at the back
                             numTwin = int(resRoomType.split(',')[1].split()[0]) # [2 x Single Bed Room, 3 x Twin Bed Room]: numTwin = 3
-
-                    if row[9] == 'Smart Flex':
-                        row[5] = row[9] # place row 9 into row 5 - row 5 is always empty is 'Smart Flex' is in row 9
+                            
+                    
+                    
+                    # if row[9] == 'Smart Flex':
+                        # place row 9 into row 5 - row 5 is always empty is 'Smart Flex' is in row 9
+                        # row[5] = row[9]
                         
-                    convertedCheckInDate = datetime.strptime(row[2], '%d-%b-%y')
-                    convertedCheckOutDate = datetime.strptime(row[3], '%d-%b-%y')
+                    convertedCheckInDate = datetime.strptime(row[2], '%b %d, %Y')
+                    convertedCheckOutDate = datetime.strptime(row[3], '%b %d, %Y')
+                    
+                    for i in range(9):
+                        print(rowHeader[i], row[i])
                     
                     # create a new bookingcom reservation object using the extracted data
                     importBookingcomRes = bookingcomReservations.objects.create(
@@ -71,10 +91,10 @@ class Command(BaseCommand):
                         checkOutDate=convertedCheckOutDate,
                         numSingle=numSingle,
                         numTwin=numTwin,
-                        totalPayment=row[6],
-                        commission=row[7],
-                        extReservationId=row[8],
-                        reservationStatus=row[9],
+                        totalPayment=row[5],
+                        commission=row[6],
+                        extReservationId=row[7],
+                        reservationStatus=row[8],
                         listedWebsites = bookingWebsite.objects.get(websiteName = "Booking.com"),
                         importTime=timezone.now(),
                     )
